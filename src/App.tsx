23e4ba69c02165374d57117dcf8d5d3943f38c6a
datuchela <davit.narimanidze408@ens.tsu.edu.ge>
2022-12-18 02:52:42 +0400

    added Toolbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
 import React, { useState } from "react";
 import Canvas from "./components/Canvas";
 import LayerList from "./components/LayerList";
+import Toolbar from "./components/Toolbar";
 import { useLayers } from "./hooks/useLayersStore";
 
 function App() {
   const { layers } = useLayers();
 
   return (
-    <div className="p-8 flex justify-between">
-      <div className="">
-        {layers?.map((layer) => (
-          <Canvas key={layer.id} />
-        ))}
+    <div>
+      <Toolbar />
+      <div className="p-8 flex justify-between w-full">
+        <div className="relative w-[1280px] h-[720px] aspect-video border border-neutral">
+          {layers?.map((layer) => (
+            <Canvas key={layer.id} layer={layer} />
+          ))}
+        </div>
+        <LayerList />
       </div>
-      <LayerList />
     </div>
   );
 }
---
+++ src/components/Toolbar.tsx
@@ -0,0 +1,56 @@
+import { useLayers } from "../hooks/useLayersStore";
+import { useLine } from "../hooks/useLineStore";
+
+const Toolbar = () => {
+  const { currentLayerId, changeLayerBackground } = useLayers();
+  const { lineColor, lineWidth, setLineColor, setLineWidth } = useLine();
+  return (
+    <header className="px-8 py-4 border-b border-neutral">
+      <div className="flex items-center gap-12">
+        <input
+          type="color"
+          className="w-6 h-6 bg-transparent cursor-pointer"
+          name="lineColor"
+          id="lineColor"
+          value={lineColor}
+          onChange={(e) => setLineColor(e.target.value)}
+        />
+        <div className="flex items-center gap-2">
+          <input
+            type="number"
+            className="min-w-[6ch] max-w-[6ch] px-1"
+            min={1}
+            max={64}
+            value={lineWidth}
+            onChange={(e) => setLineWidth(parseInt(e.target.value))}
+            required
+          />
+          <input
+            type="range"
+            // className="w-24"
+            name="lineWidth"
+            id="lineWidth"
+            min={1}
+            max={64}
+            value={lineWidth}
+            onChange={(e) => setLineWidth(parseInt(e.target.value))}
+          />
+        </div>
+        <button
+          className="px-2 border border-white"
+          onClick={() => changeLayerBackground(currentLayerId, lineColor)}
+        >
+          Fill
+        </button>
+        <button
+          className="px-2 border border-white"
+          onClick={() => changeLayerBackground(currentLayerId, lineColor)}
+        >
+          Clear
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Toolbar;
