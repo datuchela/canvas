e859e79df265987a124e57e94ccc13025d8a5443
datuchela <davit.narimanidze408@ens.tsu.edu.ge>
2022-12-18 02:54:32 +0400

    added radios in layerlist

--- src/components/LayerList.tsx
+++ src/components/LayerList.tsx
@@ -3,18 +3,21 @@ import { useLayers } from "../hooks/useLayersStore";
 
 const LayerList = () => {
   const [input, setInput] = useState("");
-  const { layers, addLayer, removeLayer } = useLayers();
+  const { layers, addLayer, removeLayer, setCurrentLayerId, currentLayerId } =
+    useLayers();
 
   function handleAddLayer(e: React.FormEvent) {
     e.preventDefault();
-    addLayer({
+    const newLayer = {
       id: layers[0]?.id + 1 || 1,
       name: input,
       z: layers[0]?.z + 1 || 1,
       hidden: false,
       ownerId: 1,
-    });
+    };
+    addLayer(newLayer);
     setInput("");
+    setCurrentLayerId(newLayer.id);
   }
 
   function handleDeleteLayer(id: number) {
@@ -36,15 +39,31 @@ const LayerList = () => {
       </form>
       <ul>
         {layers?.map((layer) => (
-          <li className="flex items-center justify-between">
-            <span>{layer.name}</span>
-            <button
-              className="border px-2 border-white"
-              onClick={() => handleDeleteLayer(layer.id)}
-            >
-              -
-            </button>
-          </li>
+          <label key={layer.id} htmlFor={layer.id.toString()}>
+            <li className="flex items-center justify-between gap-2">
+              <input
+                type="radio"
+                name="currentLayerId"
+                id={layer.id.toString()}
+                value={layer.id}
+                onChange={() => setCurrentLayerId(layer.id)}
+                checked={layer.id === currentLayerId}
+              />
+              <img
+                className="aspect-video border border-neutral"
+                src={layer?.data}
+                width={32}
+                height={18}
+              />
+              <span className="flex-1">{layer.name}</span>
+              <button
+                className="border px-2 border-white"
+                onClick={() => handleDeleteLayer(layer.id)}
+              >
+                -
+              </button>
+            </li>
+          </label>
         ))}
       </ul>
     </div>
