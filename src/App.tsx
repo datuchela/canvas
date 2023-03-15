fd9a5ccb9a809d67064650f7d14b4ca66b449a05
datuchela <davit.narimanidze408@gmail.com>
2023-03-16 02:48:54 +0400

    added pixel density

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Canvas from "./components/molecules/Canvas";
 import LayerPanel from "./components/LayerPanel";
 import Toolbar from "./components/Toolbar";
 import { useLayers } from "./hooks/useLayersStore";
 import { usePreferences } from "./hooks/usePreferStore";
 
+export const pixelDensity = 1.5;
+
+function FrameSize({
+  setFrameSize,
+}: {
+  setFrameSize: React.Dispatch<{ width: number; height: number }>;
+}) {
+  const [width, setWidth] = useState<number>();
+  const [height, setHeight] = useState<number>();
+  return (
+    <form
+      className="flex flex-col gap-4 items-center justify-center min-h-screen"
+      onSubmit={(e) => {
+        e.preventDefault();
+        if (!width || !height) return;
+        setFrameSize({ width: width, height: height });
+      }}
+    >
+      <label htmlFor="width">width:</label>
+      <input type="number" name="width" onChange={(e) => setWidth(parseInt(e.target.value))} />
+      <label htmlFor="height">height:</label>
+      <input type="number" name="height" onChange={(e) => setHeight(parseInt(e.target.value))} />
+      <button>Get Started</button>
+    </form>
+  );
+}
+
 function App() {
+  const [frameSize, setFrameSize] = useState<{ width: number; height: number }>();
   const { theme } = usePreferences();
   const { layers } = useLayers();
 
@@ -16,15 +44,33 @@ function App() {
 
   return (
     <div>
-      <Toolbar />
-      <div className="p-8 flex justify-between w-full">
-        <div className="relative w-[1280px] aspect-video border border-neutral overflow-hidden">
-          {layers?.map((layer) => (
-            <Canvas key={layer.id} layer={layer} />
-          ))}
-        </div>
-        <LayerPanel />
-      </div>
+      {!frameSize ? (
+        <FrameSize setFrameSize={setFrameSize} />
+      ) : (
+        <>
+          <Toolbar />
+          <div className="p-8 flex justify-between w-full">
+            <div
+              className="relative border border-neutral overflow-hidden"
+              style={{
+                aspectRatio: frameSize.width / frameSize.height,
+                width: `${frameSize.width / pixelDensity}px`,
+                height: `${frameSize.height / pixelDensity}px`,
+              }}
+            >
+              {layers?.map((layer) => (
+                <Canvas
+                  key={layer.id}
+                  layer={layer}
+                  canvasWidth={frameSize?.width}
+                  canvasHeight={frameSize?.height}
+                />
+              ))}
+            </div>
+            <LayerPanel />
+          </div>
+        </>
+      )}
     </div>
   );
 }
