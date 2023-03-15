a5d78caa65a25e890be94f08005fe1b7db7725a2
datuchela <davit.narimanidze408@gmail.com>
2023-03-16 02:50:34 +0400

    added variable aspect ratio and more

--- src/components/molecules/Canvas.tsx
+++ src/components/molecules/Canvas.tsx
@@ -1,41 +1,61 @@
+import { useEffect } from "react";
 import { useDraw } from "../../hooks/useDraw";
 import { useLayers } from "../../hooks/useLayersStore";
 import { useLine } from "../../hooks/useLineStore";
 import { drawLine } from "../../utils/drawLine";
 
 type CanvasType = {
-  layer: Layer;
+	layer: Layer;
+	canvasWidth: number;
+	canvasHeight: number;
 };
 
-const Canvas = ({ layer }: CanvasType) => {
-  const { currentLayerId } = useLayers();
-  const { lineWidth, lineColor } = useLine();
-  const { canvasRef, onMouseDown } = useDraw(createLine, layer);
+const Canvas = ({
+	layer,
+	canvasWidth,
+	canvasHeight,
+}: CanvasType) => {
+	const { currentLayerId, addLayerRef } = useLayers();
+	const { lineWidth, lineColor } = useLine();
+	const { canvasRef, onMouseDown } = useDraw(
+		createLine,
+		layer
+	);
 
-  function createLine({ prevPoint, currentPoint, ctx }: Draw) {
-    drawLine({
-      prevPoint,
-      currentPoint,
-      ctx,
-      lineColor: lineColor,
-      lineWidth: lineWidth,
-    });
-  }
+	useEffect(() => {
+		addLayerRef(canvasRef);
+	}, []);
 
-  return (
-    <canvas
-      className={"top-0 left-0 absolute aspect-video w-full"}
-      style={{
-        zIndex: layer.z,
-        pointerEvents: layer.id === currentLayerId ? "auto" : "none",
-      }}
-      width={1920}
-      height={1080}
-      ref={canvasRef}
-      onMouseDown={onMouseDown}
-      hidden={layer.hidden}
-    />
-  );
+	function createLine({
+		prevPoint,
+		currentPoint,
+		ctx,
+	}: Draw) {
+		drawLine({
+			prevPoint,
+			currentPoint,
+			ctx,
+			lineColor: lineColor,
+			lineWidth: lineWidth,
+		});
+	}
+
+	return (
+		<canvas
+			className={"top-0 left-0 absolute w-full"}
+			style={{
+				aspectRatio: canvasWidth / canvasHeight,
+				zIndex: layer.z,
+				pointerEvents:
+					layer.id === currentLayerId ? "auto" : "none",
+			}}
+			width={canvasWidth}
+			height={canvasHeight}
+			ref={canvasRef}
+			onMouseDown={onMouseDown}
+			hidden={layer.hidden}
+		/>
+	);
 };
 
 export default Canvas;
